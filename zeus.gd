dfa4f51304df5fb6bed5a8e94daa0778b149cc64
Ramone Henry <ramoneh94@gmail.com>
2025-12-06 18:01:04 -0500

    add zeus; add zeus intro taunt; shrink bird and bird hitbox

---
+++ zeus.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+func introTaunt() -> void:
+	var intro = get_tree().current_scene.get_node("ZeusIntro")
+	await get_tree().create_timer(10).timeout
+	intro.play()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Start invsible
+	modulate.a = 0.0
+	
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 1.0, 30.0).from(0.0)
+	introTaunt()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
