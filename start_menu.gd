c64e2233a0721f6555131659ccbc9b00e932fce9
Ramone Henry <ramoneh94@gmail.com>
2025-12-17 07:56:27 -0500

    add paper scroll sound

--- start_menu.gd
+++ start_menu.gd
@@ -1,10 +1,13 @@
 extends AnimatedSprite2D
 
-
+@onready var startMenu = $"."
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position.x = 200
 	position.y = -200
+	get_tree().current_scene.get_node("Control/Paper").play()
+	#await get_tree().create_timer(0.2).timeout
+	startMenu.play()
 	
 
 
