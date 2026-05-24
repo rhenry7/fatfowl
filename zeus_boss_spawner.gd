91dfee6aed95ebfae250cbcfa0dd26d75f678b33
Ramone Henry <ramoneh94@gmail.com>
2026-05-24 18:14:49 -0400

    Add zeus boss animations

---
+++ zeus_boss_spawner.gd
@@ -0,0 +1,13 @@
+extends Node
+
+const ZEUS_BOSS_HEAD_SCENE = preload("res://zeus_boss.tscn")
+const ATTACKS = ["eyebeam", "spitball", "barfbeam"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
